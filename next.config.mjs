32da565c9e2539f7a34997b7eb650a6e5ace066c
ArnetteLamar <arnettelamar@gmail.com>
2026-09-01 12:08:11 +0000

    Include messages in Vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/*": ["./messages/**/*"],
+  },
+};
 
 export default nextConfig;
